12b35f305d7845cdacbb5747e7ad856531f15d9f
Isichenko Ruslan <ricarrdo1488@gmail.com>
2023-11-05 15:40:59 +0300

    LR№3 task№1/3

--- src/components/LR3.vue
+++ src/components/LR3.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="block">
-
+    <div class="block__text">
+      ЛР№1
+    </div>
+    <div class="block__factorial">
+      Задание №1: Составьте алгоритм и программу с использованием языка программирования JavaScript для расчета
+      факториала натурального числа
+      <Factorial/>
+    </div>
   </div>
 </template>
 
 <script setup>
+import Factorial from "./task_components/factorial.vue";
 
 </script>
 
 <style lang="scss" scoped>
 .block {
   height: 84vh;
+
+  &__text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin: 2rem 0;
+  }
+
+  &__factorial {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    text-align: justify;
+
+    width: 60%;
+
+    margin: 2rem auto;
+  }
 }
 </style>
---
+++ src/components/task_components/factorial.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="factorial">
+    <div class="factorial__block">
+      <div class="factorial__block-text">
+        Ввод:
+      </div>
+      <input class="factorial__block-input" type="number" v-model="factorial">
+    </div>
+    <div class="factorial__number">Ответ: {{ calcFactorial(factorial) }}</div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let factorial = ref(0)
+
+function calcFactorial(num) {
+  let answer = 1;
+  if (num === 0 || num === 1) {
+    return answer;
+  } else if (num > 170) {
+    return "Бесконечность"
+  } else if (num > 1) {
+    for (let i = num; i >= 1; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  } else {
+    return "Число не подходит!"
+  }
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+.factorial {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 2rem 0;
+
+  &__block {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin: 0 0 1rem 0;
+
+    &-text {
+
+    }
+
+    &-input {
+      width: 5rem;
+      border: #212121 solid 3px;
+      background-color: #363636;
+      color: #e5e5e5;
+      border-radius: 0.5rem;
+      margin: 0 0 0 1rem;
+
+      font-family: Marmelad, sans-serif;
+      font-size: 1.5rem;
+    }
+  }
+
+
+
+  &__number {
+    display: flex;
+
+  }
+
+}
+
+</style>
